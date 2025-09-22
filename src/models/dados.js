1e5b1385debc738c7ee9f66e3b4351f11d8c3c50
Melfreitass <melissa.g.freitas@aluno.senai.br>
2025-09-22 08:53:07 -0300

    feat: preenche o arquivo dados.js com youtubers

---
+++ src/models/dados.js
@@ -0,0 +1,134 @@
+const youtubers = [
+    {
+        "id": 1,
+        "nome": "MrBeast",
+        "canal": "MrBeast",
+        "categoria": "entretenimento",
+        "inscritos": "300M+",
+        "videos": 791,
+        "vizualizacoes": "50B+",
+        "ultimoVideo": "2024-09-21",
+        "pais": "Estados Unidos"
+      },
+      {
+        "id": 2,
+        "nome": "PewDiePie",
+        "canal": "PewDiePie",
+        "categoria": "games e vlogs",
+        "inscritos": "111M+",
+        "videos": 4768,
+        "vizualizacoes": "29B+",
+        "ultimoVideo": "2024-09-15",
+        "pais": "Suécia"
+      },
+      {
+        "id": 3,
+        "nome": "Mark Rober",
+        "canal": "Mark Rober",
+        "categoria": "educativo",
+        "inscritos": "27M+",
+        "videos": 96,
+        "vizualizacoes": "3B+",
+        "ultimoVideo": "2024-09-19",
+        "pais": "Estados Unidos"
+      },
+      {
+        "id": 4,
+        "nome": "Felipe Neto",
+        "canal": "Felipe Neto",
+        "categoria": "vlogs e humor",
+        "inscritos": "46M+",
+        "videos": 5214,
+        "vizualizacoes": "16B+",
+        "ultimoVideo": "2024-09-20",
+        "pais": "Brasil"
+      },
+      {
+        "id": 5,
+        "nome": "AuthenticGames",
+        "canal": "AuthenticGames",
+        "categoria": "games",
+        "inscritos": "21M+",
+        "videos": 7500,
+        "vizualizacoes": "12B+",
+        "ultimoVideo": "2024-09-22",
+        "pais": "Brasil"
+      },
+      {
+        "id": 6,
+        "nome": "Jovem Nerd",
+        "canal": "Jovem Nerd",
+        "categoria": "cultura pop",
+        "inscritos": "2M+",
+        "videos": 1800,
+        "vizualizacoes": "300M+",
+        "ultimoVideo": "2024-09-21",
+        "pais": "Brasil"
+      },
+      {
+        "id": 7,
+        "nome": "Nerdologia",
+        "canal": "Nerdologia",
+        "categoria": "educativo",
+        "inscritos": "4M+",
+        "videos": 700,
+        "vizualizacoes": "500M+",
+        "ultimoVideo": "2024-09-18",
+        "pais": "Brasil"
+      },
+      {
+        "id": 8,
+        "nome": "Whindersson Nunes",
+        "canal": "Whindersson Nunes",
+        "categoria": "humor e vlogs",
+        "inscritos": "44M+",
+        "videos": 500,
+        "vizualizacoes": "4B+",
+        "ultimoVideo": "2024-08-30",
+        "pais": "Brasil"
+      },
+      {
+        "id": 9,
+        "nome": "Manual do Mundo",
+        "canal": "Manual do Mundo",
+        "categoria": "ciência e DIY",
+        "inscritos": "18M+",
+        "videos": 1800,
+        "vizualizacoes": "4B+",
+        "ultimoVideo": "2024-09-22",
+        "pais": "Brasil"
+      },
+      {
+        "id": 10,
+        "nome": "TED",
+        "canal": "TED",
+        "categoria": "palestras e ideias",
+        "inscritos": "25M+",
+        "videos": 4500,
+        "vizualizacoes": "2B+",
+        "ultimoVideo": "2024-09-21",
+        "pais": "Internacional"
+      }
+];
+
+export dafault { youtubers }
+
+//"id": 11,
+//"nome": "Luccas Neto",
+//"canal": "LUCCAS TOON",
+//"categoria": "infantil e educativo",
+//"inscritos": "53M+",
+//"videos": 3328,
+//"vizualizacoes": "33B+",
+//"ultimoVideo": "2024-09-22",
+//"pais": "Brasil"
+
+//"id": 12,
+//"nome": "Canal do Gnomo",
+//"canal": "Canal do Gnomo",
+//"categoria": "humor e animação",
+//"inscritos": "1M+",
+//"videos": 150,
+//"vizualizacoes": "100M+",
+//"ultimoVideo": "2024-09-19",
+//"pais": "Brasil"
